20be7cebf7c89446b237e78d960c58a5fc530bb4
0trava <102797527+0trava@users.noreply.github.com>
2023-02-09 13:47:54 +0000

    Deploying to gh-pages from @ 0trava/goit-js-hw-09@4376d9e5b79afc852044a30c71dabf4a59db9000 🚀

---
+++ 01-color-switcher.3bd04529.js
@@ -0,0 +1,2 @@
+document.querySelector("body");const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,colorInterval=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(colorInterval)}));
+//# sourceMappingURL=01-color-switcher.3bd04529.js.map
